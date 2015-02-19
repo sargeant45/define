a4af22a7525f6daf7dab5cc76c8f8af981f0bb5d
Ethan Arterberry <ethanarterberry@gmail.com>
2015-02-19 13:01:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function define() {
   var word = document.getElementById('word').text;
   var url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=6171866a-33a7-4f70-b763-b8ffe73184b2"
-  var raw = $.get( url, function( data ) {
-  alert( "Data Loaded: " + data );
-  });
-  var def = raw.def;
+  
+  
   document.getElementById('define').innerHTML = def;
 }
