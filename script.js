9b0468a038f7735100e8f4869f649c37154f6eab
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-10 02:55:59 -0700

    Added CORS request

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function define() {
   var word = $("#wow").val();
   var url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=6171866a-33a7-4f70-b763-b8ffe73184b2"
   
+  // CORS requesting time!
+  var request = createCORSRequest( "get", url);
+  if ( request ){
+      // Define a callback function
+      request.onload = function(){};
+      // Send request
+      request.send();
+  }
+  
   // Use Jquery to grab from dictionary and change elements when done.
   $.ajax({
   url: url,
