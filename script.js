1f2528955d7ba283b6f8ec98deeb53cbc38153f5
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-10 02:32:49 -0700

    Fix word def

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function define() {
-  var word = document.getElementById('word').text;
+  var word = document.getElementById('word').value;
   var url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=6171866a-33a7-4f70-b763-b8ffe73184b2"
   
   // Use Jquery to grab from dictionary and change elements when done.
