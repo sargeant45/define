578c8ce72e41ed5227bc744bf762f344fc15e6f3
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-10 02:58:07 -0700

    Finished CORS request thing

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function define() {
-  var word = $("#wow").val();
+  var word = $("#word").val();
   var url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=6171866a-33a7-4f70-b763-b8ffe73184b2"
   
   // CORS requesting time!
@@ -24,4 +24,19 @@ function define() {
     }
   });
   
+}
+
+function createCORSRequest(method, url){
+  // CORS request method thingy I found on http://jquery-howto.blogspot.com/2013/09/jquery-cross-domain-ajax-request.html#cors
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr){
+        // XHR has 'withCredentials' property only if it supports CORS
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){ // if IE use XDR
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
 }
