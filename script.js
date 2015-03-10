692152cdbed76d34945164be77a1255a8e1cfb35
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-10 02:43:44 -0700

    fixed crossdomain

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function define() {
   var url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=6171866a-33a7-4f70-b763-b8ffe73184b2"
   
   // Use Jquery to grab from dictionary and change elements when done.
-  $.get( url, function( data ) {
+  $.get( url, function( data ), crossDomain: true {
     alert( "Definition data loaded: " + data );
     $("#word").html(data.hw);
     $("#pronunciation").html(data.pr);
