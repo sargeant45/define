86cc784c5dabb31d0a51bccdc9f003c4e1b20a4c
Ethan Arterberry <ethanarterberry@gmail.com>
2015-03-10 02:49:36 -0700

    it should probably maybe sort of work now

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ function define() {
   var url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=6171866a-33a7-4f70-b763-b8ffe73184b2"
   
   // Use Jquery to grab from dictionary and change elements when done.
-  $.get( url, function( data ), crossDomain: true {
-    alert( "Definition data loaded: " + data );
+  $.ajax({
+  url: url,
+  crossDomain: true,
+  success: function (data) {
+    console.log(data)
+    alert(data);
     $("#word").html(data.hw);
     $("#pronunciation").html(data.pr);
     $("#define").html(data.def);
-  });
+  }
+});
   
 }
